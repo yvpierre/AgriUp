98b7395190a28115644bab5cb265ec24b9abe980
Pierre <88279582+yvpierre@users.noreply.github.com>
2022-10-04 11:33:38 +0200

    infos + btn homepage

--- agriup/src/pages/Home.js
+++ agriup/src/pages/Home.js
@@ -10,6 +10,19 @@ const Home = () => {
             <div className={"heroBanner"}>
                 <h1>Économisez votre eau.<br/>Optimisez votre exploitation</h1>
             </div>
+            <div className={"body"}>
+                <div className={"body--pres"}>
+                    <div className={"body--presText"}>L'eau est une denrée inestimable. Notre vie est étroitement liée à cette composition d'oxygène et d'hydrogène, notamment notre alimentation. Entre 70 et 80 % de la consommation mondiale d'eau est utilisée pour l'agriculture. C'est pourquoi l'économie d'eau dans l'agriculture et son utilisation sont deux des questions les plus importantes de ces dernières années.<br/><br/>
+                        Agri’Up est une idée novatrice pour révolutionner l’univers agricole. Dans un monde où l’écologie prend une place de plus en plus importante, il est nécessaire de revoir notre consommation d’eau.
+                    </div>
+                    <div className={"body--presRight"}>
+                        <div className={"body--presText2"}>C’est pour cela que nous proposons une solution afin d’accompagner les agriculteurs. Sous la forme d’un simulateur, l’agriculteur peut rentrer toutes les données nécessaires pour connaitre la consommation d’eau optimal dont il a besoin pour sa production.</div>
+                        <div className={"body--presImg"}>
+                            <div className={"body--presBtn"}>Bouton</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
