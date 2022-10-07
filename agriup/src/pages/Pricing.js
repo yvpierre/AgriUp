a2f3c9877656aecc34b462f6bd6e2160d1fd5683
SangminSHIM <tkdals4723@gmail.com>
2022-10-07 10:06:09 +0200

    ok

--- agriup/src/pages/Pricing.js
+++ agriup/src/pages/Pricing.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navigation from '../composants/Navigation';
 import Footer from '../composants/Footer';
 import Prices from '../composants/Prices';
+import Co from '../composants/Co';
 
 const Pricing = () => {
     return (
         <div>
             <Navigation />
             <Prices />
+            <Co />
             <Footer />
         </div>
     );
